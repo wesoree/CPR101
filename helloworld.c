59946cbc09ded9b8713145ce3caf0bb572ca1e29
wesoree <wesoree1@gmail.com>
2025-05-05 12:50:11 -0400

    update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -2,5 +2,10 @@
 
 int main(void){
 	printf("hello world\n");
+	//a comment
+	printf("I came to Seneca to take on a major in CPA, I back up my files using Github at times, depending on filesize");
+
+	printf("on local files I use a backup program.");
+
 	return 0;
 }
